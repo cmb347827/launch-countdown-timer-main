05eda6296844a786f097ea1d94bd86215057c8fc
cmb347827 <v38d422ll621dem@gmail.com>
2025-03-23 07:20:41 -0400

    Initial commit

---
+++ launch-countdown-timer-main.js
@@ -0,0 +1,26 @@
+'use strict'; 
+
+$(window).resize(function(){
+	
+});
+
+
+const data ={
+	
+}
+
+
+function addListener(){
+	// Set up event listeners for next and previous buttons
+    //document.getElementById('nextBtn').addEventListener('click', nextSlide);
+    //document.getElementById('prevBtn').addEventListener('click', previousSlide);
+}
+
+
+
+
+$(window).on('load',function(){
+	
+    
+});
+
