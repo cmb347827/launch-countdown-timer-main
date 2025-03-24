cb2a5f6c9a8cc8e10242d18284333a6555d11e56
cmb347827 <v38d422ll621dem@gmail.com>
2025-03-24 06:14:42 -0400

    Added start of js code, added some basic changes to the look of the number display squares.

--- launch-countdown-timer-main.js
+++ launch-countdown-timer-main.js
@@ -6,7 +6,10 @@ $(window).resize(function(){
 
 
 const data ={
-	
+	 days: document.getElementById('days'),
+     hours: document.getAnimations('hours'),
+     minutes:document.getElementById('minutes'),
+     seconds:document.getElementById('seconds'),
 }
 
 
@@ -16,11 +19,15 @@ function addListener(){
     //document.getElementById('prevBtn').addEventListener('click', previousSlide);
 }
 
-
+const countDown=(days,hours,minutes,seconds)=>{
+    //start countdown . At first start becomes days-1, 24 hours, 00 minutes, 00 seconds.
+    data.days.textContent = Number(days);
+}
 
 
 $(window).on('load',function(){
-	
+    //initialize with start of 14 days.
+	countDown('14','00','00','00');
     
 });
 
