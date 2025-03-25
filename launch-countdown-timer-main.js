e1b81d71010afbe214d51f0ace72edc4eea4f899
cmb347827 <v38d422ll621dem@gmail.com>
2025-03-25 06:14:47 -0400

    start of counter code

--- launch-countdown-timer-main.js
+++ launch-countdown-timer-main.js
@@ -6,28 +6,87 @@ $(window).resize(function(){
 
 
 const data ={
-	 days: document.getElementById('days'),
-     hours: document.getAnimations('hours'),
-     minutes:document.getElementById('minutes'),
-     seconds:document.getElementById('seconds'),
+	 display_days: document.getElementById('days'),
+     display_hours: document.getAnimations('hours'),
+     display_minutes:document.getElementById('minutes'),
+     display_seconds:document.getElementById('seconds'),
+     daysMilliseconds: 0,
+     startTime: 0,
+     timePassed:0,
+     timeLeft:0,
+     daysLeft: 0,
+     hoursLeft:0,
+     minutesLeft:0,
+     secondsLeft:0,
+     reachedZero: false,
+
 }
 
 
-function addListener(){
-	// Set up event listeners for next and previous buttons
-    //document.getElementById('nextBtn').addEventListener('click', nextSlide);
-    //document.getElementById('prevBtn').addEventListener('click', previousSlide);
+const addLeadingZero=(string,length)=>{
+    return string.padStart(length,'0');
+}
+const displayCountdown=(days,hours,minutes,seconds)=>{
+    data.display_days.textContent = addLeadingZero(days,2);
+    data.display_hours.textContent = addLeadingZero(hours,2);
+    data.display_minutes.textContent = addLeadingZero(minutes,2);
+    data.display_seconds.textContent = addLeadingZero(seconds,2);
+}
+
+const timePassed=()=>{
+    //currentime - startime
+    const currentdate = new Date();
+    const currentime = currentdate.getTime();
+    data.timePassed = currentime - data.startTime;
+}
+const updateMilliseconds=()=>{
+     //data.daysMilliseconds = calculateMilliseconds(days) line 85.
+     //difference between calculateMilliseconds(days) and timePassed()
+      data.timeLeft = data.daysMilliseconds - data.timePassed;             
+     //displayCountdown  , call with string values for days,hours,minutes and seconds.
+}
+const calculateMilliseconds=(days)=>{
+    //1 day: 24 hours : 24 * 60 = 1440 minutes * 60 = 86400 seconds * 1000 = 86400000 milliseconds
+    //so total number of milliseconds in number of (days) parameter.
+    const millisecondsDays = parseInt(days) * 86400000;
+    return millisecondsDays;
+}
+
+const updateValues=()=>{
+    console.log(data.timeLeft);
+    //convert milliseconds to days , hours, minutes, and seconds remaining.
 }
 
 const countDown=(days,hours,minutes,seconds)=>{
     //start countdown . At first start becomes days-1, 24 hours, 00 minutes, 00 seconds.
-    data.days.textContent = Number(days);
+    displayCountdown(days,hours,minutes,seconds);
+    
+    setInterval(() => {
+        if(!data.reachedZero){
+            //asynchronous calls ...............
+            timePassed();
+            updateMilliseconds();
+            updateValues();
+        } else if(data.reachedZero){
+            //should be all zeros.
+            displayCountdown(data.days,data.hours,data.minutes,data.seconds); 
+        }
+       
+    }, 1000);
+
+   
+
+
 }
 
 
 $(window).on('load',function(){
-    //initialize with start of 14 days.
-	countDown('14','00','00','00');
     
+    const now= new Date();
+    data.startTime = now.getTime();
+    //initialize with start of 14 days.
+    data.daysMilliseconds = calculateMilliseconds('14');
+	countDown('14','00','00','00');
+    //test with other start value.
 });
 
